f81ae57c05ca6f318eafa77efa96e66725accfa0
Miguel Henrique <miguelhenriqueisaac@gmail.com>
2025-03-26 22:22:21 -0300

    Itens/soma

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,11 +1,23 @@
 function calcular(){
-    // recupera o valor da ação social digitado
     // declaração de variável sem tipo
+    
+    // recupera o valor da ação social digitado
+    let soma = 0
+
     let acaoSocial = document.getElementById("acaoSocial").value
-    // recupera o calor da homenagem digitado
+    soma = soma + Number(acaoSocial)
+    // recupera o valor da homenagem digitado
     let homenagem = document.getElementById("homenagem").value
-    // soma de valores 
-    let soma = Number(acaoSocial) + Number(homenagem) 
-    // alert para dizer o que há na variável
-    alert(soma)
+    soma = soma + Number(homenagem)
+    // '' valor do leite
+    let leite = document.getElementById("leite").value
+    soma = soma + Number(2 * Number(leite))
+
+    let kitAvulso = document.getElementById("kitAvulso").value
+    soma = soma + Number(30 * kitAvulso)
+    
+    let suplementoAvulso = document.getElementById("suplementoAvulso").value
+    soma = soma + Number(15 * Number(suplementoAvulso))
+    //devolve o resultadop para HTML
+    document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
