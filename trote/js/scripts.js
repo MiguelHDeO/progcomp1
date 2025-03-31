327467ca79444b93e2c780003e5f3abacd090c0b
Miguel Henrique <miguelhenriqueisaac@gmail.com>
2025-03-31 20:44:19 -0300

    -avulso

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -13,14 +13,37 @@ function calcular(){
     let leite = document.getElementById("leite").value
     soma = soma + Number(2 * Number(leite))
 
-    let kitAvulso = document.getElementById("kitAvulso").value
-    soma = soma + Number(30 * kitAvulso)
+    let suplemento = document.getElementById("suplemento").value
+    soma = soma + Number(15 * Number(suplemento))
+
+    let kit = Number(document.getElementById("kit").value)
+    soma = soma + Number(30 * kit)
+    //recupera cor da equipe
+
+    let equipe = document.getElementById("equipe").value
+    let pontosKitSup = 0 // guarda pontuação do kit
+    if (equipe == "Laranja"){
+        // esquipe é laranja
+        if  (kit >= 97 && suplemento >=49){
+            pontosKitSup = 5000 + ((kit - 97)* 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >=39){
+            pontosKitSup = 4000 + ((kit - 78)* 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >=25){
+            pontosKitSup = 2500 + ((kit - 49)* 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >=19 && suplemento >=10){
+            pontosKitSup = 1000 + ((kit - 19)* 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit < 19 && suplemento <10){
+            pontosKitSup = (kit * 30) + ((suplemento * 15))
+        }
+    }
+    alert(pontosKitSup)    
     
     let oleo = document.getElementById("oleo").value
     soma = soma + Number(oleo)
-
-    let suplementoAvulso = document.getElementById("suplementoAvulso").value
-    soma = soma + Number(15 * Number(suplementoAvulso))
 
     let macarrao = document.getElementById("macarrao").value
     soma = soma + (0.5 * Number(macarrao))
@@ -37,6 +60,6 @@ function calcular(){
     let feijão1kg = document.getElementById("feijão1kg").value
     soma = soma + Number(feijão1kg)
 
-    //devolve o resultadop para HTML
+    //devolve o resultado para HTML
     document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
