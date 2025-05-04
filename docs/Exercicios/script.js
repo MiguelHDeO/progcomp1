7fbf7373003702785f9e832cab8e8ba1c58cc75c
Miguel Henrique <miguelhenriqueisaac@gmail.com>
2025-05-04 20:41:24 -0300

    while

--- docs/Exercicios/script.js
+++ docs/Exercicios/script.js
@@ -1,13 +1,38 @@
- function oxe0(){
-    let nota //guarda as notas
-    let conta = 1; //conta de 1 a 6
-    let soma = 0
-    while (conta<=6){
-        nota = Number(prompt(`Informe a nota ${conta}`));
-        soma = soma + nota;
-        conta++; //conta = conta + 1
+ function pesquisaSatisfacao(){
+   let totalNotas = 0;
+   let satisfeitos = 0;
+   let insatisfeitos = 0;
+   let contador = 0;
+
+   while(contador < 10 ){
+    let nota = window.prompt(`Participante ${contador + 1}: Digite uma nota referente a sua satisfação (0 a 10)`);
+
+    if (nota < 0 || nota > 10){
+        alert (`Nota invalida, digite um número entre 0 e 10 `);
+        continue;
     }
-    let media = soma /6
-    //mostra o resultado
-    alert (`A media das notas é: ${media.toFixed(1)}`)
+
+    totalNotas += nota;
+
+    if (nota >= 8){
+        satisfeitos++;
+    } else if (nota <= 5 ){
+        insatisfeitos++;
+    }
+
+    contador++;
+
+    }
+
+    let media = totalNotas / 10
+
+    alert(
+        `Resultado da Pesquisa:\n` +
+        `Média das notas: ${media.toFixed(2)}\n` +
+        `Total de satisfeitos (nota >= 8): ${satisfeitos}\n` +
+        `Total de insatisfeitos (nota < 5): ${insatisfeitos}`
+    )
+
+   
+
 }
