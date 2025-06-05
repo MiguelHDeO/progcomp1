48be5c6d86a5574d86e6f9ceface446e3830f82e
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2025-06-04 21:31:14 -0300

    exe2 correcao

--- VETOR/exe1.js
+++ VETOR/exe1.js
@@ -16,21 +16,7 @@ function exe1(){
     alert(`${impares.length} números impares:  ${impares}`)
 }
 
-function exe2(){
-    let vet = [], pares = [], impares = []
 
-    for(let i = 0; i < 10; i++){
-        vet.push(Number(prompt(`informe o ${i+1}° número`)))
-    }
 
-    for(let i=0; i<10; i++){
-        if(vet[i] % 2 == 0){
-            pares.push(vet[i] + 10)
-        }else{
-            impares.push(vet[i] * 5)
-        }
-    }
 
-    console.log (`pares: ${pares}`)
-    console.log (`impares: ${impares}`)
-}
+
---
+++ VETOR/exe2.js
@@ -0,0 +1,18 @@
+function exe2(){
+    let vet = [], pares = [], impares = []
+
+    for(let i = 0; i < 10; i++){
+        vet.push(Number(prompt(`informe o ${i+1}° número`)))
+    }
+
+    for(let i=0; i<10; i++){
+        if(i % 2 == 0){
+            vet[i] = vet[i] + 10
+        }else{
+            vet[i] = vet[i] * 5
+        }
+    }
+
+    console.log (`pares: ${pares}`)
+    console.log (`impares: ${impares}`)
+}
